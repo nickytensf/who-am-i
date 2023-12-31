46ec7b7f14dfb86f8a7660bdedbac21cff31a815
laudaaakun <92017950+nickytensf@users.noreply.github.com>
2023-12-31 13:32:43 +0700

    Initial commit

---
+++ toggler.js
@@ -0,0 +1,8 @@
+// Slide
+
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
